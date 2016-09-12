7206c0c1d90fc2287c9984e9ed042d789fe2e419
Salvador Gomez <sgoliver.net@gmail.com>
2016-09-12 17:17:01 +0200

    Mapas 1

---
+++ android-mapas-1/app/src/main/java/net/sgoliver/android/mapas/MainActivity.java
@@ -0,0 +1,30 @@
+package net.sgoliver.android.mapas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class MainActivity extends AppCompatActivity
+        implements OnMapReadyCallback {
+
+    private GoogleMap mapa;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        mapa = map;
+    }
+}
